021891c130d37fd2d63f23747c34bc577c3c322b
EnzoJF6 <123915333+EnzoJF6@users.noreply.github.com>
2023-03-29 17:12:30 -0300

    Create java.js

---
+++ java.js
@@ -0,0 +1,27 @@
+function m1(){
+    document.getElementById('b1').style.display="block";
+    document.getElementById('b2').style.display="none";
+  };
+  
+  function m2(){
+    document.getElementById('b1').style.display="none";
+    document.getElementById('b2').style.display="block";
+  };
+  function m3(){
+    document.getElementById('b3').style.display="block";
+    document.getElementById('b4').style.display="none";
+  };
+  
+  function m4(){
+    document.getElementById('b3').style.display="none";
+    document.getElementById('b4').style.display="block";
+
+  };function m5(){
+    document.getElementById('b5').style.display="block";
+    document.getElementById('b6').style.display="none";
+  };
+  
+  function m6(){
+    document.getElementById('b5').style.display="none";
+    document.getElementById('b6').style.display="block";
+  };
